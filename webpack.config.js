a851f6b9db8a5824a609b53fe3c9834a1b97445b
Brian Beck <brian.beck@outlook.com>
2022-01-27 21:42:11 -0800

    Move options to source directory

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,22 @@
 const path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     mode: "production",
     entry: {
         background_scripts: "./src/background_scripts/background.js",
-        content_scripts: "./src/content_scripts/download-all.js"
+        content_scripts: "./src/content_scripts/download-all.js",
+        options: "./src/options/options.js"
     },
     output: {
         path: path.resolve(__dirname, "addon"),
         filename: "[name]/index.js"
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "src", "options", "options.html"),
+            filename: "options.html",
+            chunks: ["options"]
+        })
+    ]
 };
